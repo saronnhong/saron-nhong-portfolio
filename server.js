f1797feefdf977b0e0eb1dc2e13473f2294361f9
Saron <saronnhong@gmail.com>
2019-08-19 19:43:40 -0700

    remove options in server.js

--- server.js
+++ server.js
@@ -9,20 +9,20 @@ const nodemailer = require('nodemailer');
 require('dotenv').config();
 
 
-let transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-        user: process.env.EMAIL,
-        pass: process.env.PASSWORD
-    }
-});
+// let transporter = nodemailer.createTransport({
+//     service: 'gmail',
+//     auth: {
+//         user: process.env.EMAIL,
+//         pass: process.env.PASSWORD
+//     }
+// });
 
-let mailOptions = {
-    from: 'Micaiah20x6@gmail.com',
-    to: 'saronnhong@gmail.com',
-    subject: 'testing',
-    text: 'it works'
-};
+// let mailOptions = {
+//     from: 'Micaiah20x6@gmail.com',
+//     to: 'saronnhong@gmail.com',
+//     subject: 'testing',
+//     text: 'it works'
+// };
 
 app.get("/send-email", function(req, res) {
     
