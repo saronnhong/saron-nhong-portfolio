1138be355d1a4e014effd2915fab9ac5323a70a7
Saron <saronnhong@gmail.com>
2019-08-19 20:31:12 -0700

    remove ignore build from gitignore

---
+++ client/build/precache-manifest.0ebb5423b47f88c37fcddbba03539003.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4195635652eb4c2f9bf803af7e19ce33",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7fff59e56548c9741822",
+    "url": "/static/css/main.ec6b5d20.chunk.css"
+  },
+  {
+    "revision": "bc2e8d59b493fdfecbf3",
+    "url": "/static/js/2.36108377.chunk.js"
+  },
+  {
+    "revision": "7fff59e56548c9741822",
+    "url": "/static/js/main.4192661b.chunk.js"
+  },
+  {
+    "revision": "afafff7891452fb136ed",
+    "url": "/static/js/runtime~main.b4d6ecf7.js"
+  }
+]);
